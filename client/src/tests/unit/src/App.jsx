f7397653a1c0c212e755ed210ca0b8e5eb660bd4
silvia730 <silvianjeri730@gmail.com>
2025-07-23 18:42:21 +0300

    Create App.jsx

---
+++ client/src/tests/unit/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+import ErrorBoundary from './components/ErrorBoundary';
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <Home />
+    </ErrorBoundary>
+  );
+}
+
+export default App;
