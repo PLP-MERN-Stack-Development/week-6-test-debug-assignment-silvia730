dc1168aaa41912974523a47e8c85f57680dfa112
silvia730 <silvianjeri730@gmail.com>
2025-07-23 18:38:52 +0300

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bugRoutes = require('./routes/bugRoutes');
+const errorHandler = require('./middleware/errorHandler');
+const app = express();
+
+app.use(express.json());
+app.use('/api/bugs', bugRoutes);
+app.use(errorHandler);
+
+module.exports = app;
